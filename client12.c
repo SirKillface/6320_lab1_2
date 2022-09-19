85d1ddb9c08b53edcacfa3300ee1ffdef569fee8
paul <dalton.paul93@yahoo.com>
2022-09-19 11:04:05 -0500

    first commit

---
+++ client12.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+#define PORT 10020
+#define MAXLINE 4096
+
+int main(int argc, char **argv){
+    int sockfd, n;
+    int a, b, result, valid;
+    char choice;
+    struct sockaddr_in serv_addr;
+    char sendLine[MAXLINE], recvLine[MAXLINE];
+
+
+    if(argc != 4){
+        perror("need more arguments 2 ints and a operator");
+        exit(1);
+    }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0){
+        perror("Error opening socket");
+        exit(2);
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+        perror("Problem in connecting to the server\n");
+        exit(3);
+    }
+
+    bzero(sendLine, MAXLINE);
+
+    send(sockfd, argv[1], sizeof(int), 0);
+    send(sockfd, argv[2], sizeof(int), 0);
+    send(sockfd, argv[3], sizeof(char), 0);
+
+
+    recv(sockfd, &choice, sizeof(int), 0);
+    recv(sockfd, &a, sizeof(int), 0);
+    recv(sockfd, &b, sizeof(int), 0);
+    recv(sockfd, &result, sizeof(int), 0);
+    recv(sockfd, &valid, sizeof(int), 0);
+
+    if (valid ==1){
+        printf("%s%d%s%c%s%d%s%d%s\n", "returned message ", a, " ", choice, " ", b, " = ", result, " is valid");
+    }
+    else{
+        printf("%s\n", "is not a valid request");
+    }
+
+
+    close(sockfd);
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ server12.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 10020
+#define MAXLINE 4096
+#define LISTENQ 8
+
+int main(int argc, char *argv){
+
+    int sockfd, newSockfd, n;
+    int a, b, result, valid;
+    int flag = 0;
+    char c;
+    char buf[MAXLINE];
+
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0){
+        perror("error opening socket");
+        exit(1);
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+
+    if( bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+        perror("error binding");
+        exit(2);
+    }
+
+
+    listen(sockfd, LISTENQ);
+    printf("%s\n", "Server running...waiting for connections");
+
+    clilen = sizeof(cli_addr);
+    while(1){
+        newSockfd = accept(sockfd, (struct sockaddr *) &cli_addr, & clilen);
+
+        if(newSockfd < 0){
+            perror("accept error");
+            exit(3);
+        }
+
+
+        bzero(buf, MAXLINE);
+        n = recv(newSockfd, buf, sizeof(int), 0);
+        buf[n] = 0;
+        a = atoi(buf);
+
+        bzero(buf, MAXLINE);
+        n = recv(newSockfd, buf, sizeof(int), 0);
+        buf[n] = 0;
+        b = atoi(buf);
+
+
+        bzero(buf, MAXLINE);
+        n = recv(newSockfd, buf, sizeof(buf), 0);
+        buf[n] = 0;
+        c = *buf;
+
+
+
+        if((a == 0) || (b ==0) && (c == '/')){
+            flag = 1;
+        }
+
+        else if((b > a) && c == '-'){
+            flag = 1;
+        }
+
+        if(flag == 0){
+            if(c == '+'){
+                result = a + b;
+                valid = 1;
+            }
+            else if(c == '-'){
+                result = a - b;
+                valid = 1;
+            }
+            else if(c == 'x'){
+                result = a * b;
+                valid = 1;
+            }
+            else if(c == '/'){
+                result = a / b;
+                valid = 1;
+            }
+        }
+
+        else {
+            result = 0;
+            valid = 2;
+        }
+
+        printf("%d%s%c%s%d%s%d\n", a, " ", c, " ", b, " = ", result);
+
+        send(newSockfd, &c, sizeof(int), 0);
+        send(newSockfd, &a, sizeof(int), 0);
+        send(newSockfd, &b, sizeof(int), 0);
+        send(newSockfd, &result, sizeof(int), 0);
+        send(newSockfd, &valid, sizeof(int), 0);
+        //send(newSockfd, &result, sizeof(int), 0);
+    }
+    close(newSockfd);
+    close(sockfd);
+
+    return 0;
+}
